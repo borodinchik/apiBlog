908df5180c2335c022178b406b6757743d2c030f
borodinchik <borodin.vladymyr@gmail.com>
2018-05-05 12:46:29 +0300

    Model make relationship

---
+++ app/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'title', 'body'
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+}
+
